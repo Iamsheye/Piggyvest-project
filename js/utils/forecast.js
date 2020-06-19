3ab9143828c62e60a7bd0f98cb091601252f9f1c
Abduljafar Kamarudeen <abduljafar.kamaru@ekedp.com>
2020-06-19 08:43:03 +0100

    add-download

--- js/utils/forecast.js
+++ js/utils/forecast.js
@@ -1,3 +1,11 @@
+const forecastData = {
+    "Expected Return":"",
+    "Savings Interval": "",
+    "Product":"", 
+    "Start Date":"", 
+    "End Date":"",
+    "Recommended Savings":""
+}
 function forecast(){
     let startDate = new Date($("#startDate").val())
     let endDate = new Date($("#endDate").val())
@@ -13,6 +21,11 @@ function forecast(){
         let savingsDuration = (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)
         let expectedReturns = $("#expected_returns").val();
         let dailyInterest = 0;
+        forecastData['Savings Interval'] = $("#savings_interval").val();
+        forecastData['Product'] = $("#product").val();
+        forecastData['Start Date'] = startDate;
+        forecastData['End Date'] = endDate;
+        forecastData['Expected Return'] = expectedReturns;
         const productRate = {
             piggybank: 0.027,
             flexnaira: 0.027,
@@ -28,29 +41,33 @@ function forecast(){
                 if(savingsDuration <= 1){
                     alert("Date range must be greater than 1 day for Daily saving interval")
                 }else{
-                    document.getElementById("interval_text").innerHTML = $("#savings_interval").val()
-                    document.getElementById("recommended_saving").innerHTML = dailyRecommended.toFixed(2)
+                    document.getElementById("interval_text").innerHTML = $("#savings_interval").val();
+                    document.getElementById("recommended_saving").innerHTML = dailyRecommended.toFixed(2);
+                    forecastData['Recommended Savings'] = dailyRecommended.toFixed(2);
                 }
             } else if ($("#savings_interval").val() === "Weekly") {
                 if (savingsDuration <= 7) {
                     alert("Date range must be greater than 1 week for Weekly saving interval")
                 } else {
-                    document.getElementById("interval_text").innerHTML = $("#savings_interval").val()
-                    document.getElementById("recommended_saving").innerHTML = (dailyRecommended.toFixed(2) * 7).toFixed(2)
+                    document.getElementById("interval_text").innerHTML = $("#savings_interval").val();
+                    document.getElementById("recommended_saving").innerHTML = (dailyRecommended.toFixed(2) * 7).toFixed(2);
+                    forecastData['Recommended Savings'] = (dailyRecommended.toFixed(2) * 7).toFixed(2);
                 }
             } else if ($("#savings_interval").val() === "Monthly") {
                 if (savingsDuration <= 28) {
                     alert("Date range must be greater than 1 month for Monthly saving interval")
                 } else {
-                    document.getElementById("interval_text").innerHTML = $("#savings_interval").val()
-                    document.getElementById("recommended_saving").innerHTML = (dailyRecommended.toFixed(2) * 28).toFixed(2)
+                    document.getElementById("interval_text").innerHTML = $("#savings_interval").val();
+                    document.getElementById("recommended_saving").innerHTML = (dailyRecommended.toFixed(2) * 28).toFixed(2);
+                    forecastData['Recommended Savings'] = (dailyRecommended.toFixed(2) * 28).toFixed(2);
                 }
             } else if ($("#savings_interval").val() === "Yearly") {
                 if (savingsDuration <= 365) {
                     alert("Date range must be greater than 1 year for Yearly saving interval")
                 } else {
-                    document.getElementById("interval_text").innerHTML = $("#savings_interval").val()
-                    document.getElementById("recommended_saving").innerHTML = (dailyRecommended.toFixed(2) * 365).toFixed(2)
+                    document.getElementById("interval_text").innerHTML = $("#savings_interval").val();
+                    document.getElementById("recommended_saving").innerHTML = (dailyRecommended.toFixed(2) * 365).toFixed(2);
+                    forecastData['Recommended Savings'] = (dailyRecommended.toFixed(2) * 365).toFixed(2);
                 }
             }else{
                 alert("Invalid saving plan selected");
